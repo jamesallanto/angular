fff5a2417e14f5042a91afcb3594788d9af929f2
James Allan To <jamesto@Jamess-MacBook.local>
2017-12-13 13:47:20 +0800

    fixed error

--- angular-app2/src/app/servers/servers.component.ts
+++ angular-app2/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = "No server was created!";
   serverName = '';
   serverCreated = false;
+  isValid = false;
 
   constructor() { 
     setTimeout(() => {
@@ -26,6 +27,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.isValid = true;
     this.serverCreationStatus = "Server Created!";
   }
 
